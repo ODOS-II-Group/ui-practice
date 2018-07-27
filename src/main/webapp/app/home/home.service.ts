2f70cbc244b80924749d289a1fa012fbd3fe2499
wube kifle <wubshet.kifle@newwave.io>
2018-07-27 15:00:18 -0400

    ODOS-1: clean up dead links

--- src/main/webapp/app/home/home.service.ts
+++ src/main/webapp/app/home/home.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from "rxjs/Observable";
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 

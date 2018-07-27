9ceaa94272a5b79eb632ec5fa15cac7a95efc7d2
wube kifle <wubshet.kifle@newwave.io>
2018-07-27 17:49:52 -0400

    ODOS-2: Create functions to update an issue and get all issues.

--- src/main/webapp/app/room-issues/room-issues.service.ts
+++ src/main/webapp/app/room-issues/room-issues.service.ts
@@ -12,4 +12,23 @@ export class RoomIssuesService {
         private $localStorage: LocalStorageService,
         private $sessionStorage: SessionStorageService) { }
 
+
+        getAllIssues(){
+            return this.http.get(CRRS_API_URL + "api/room-equipment-issues");
+        }
+
+        updateAnIssue(data){
+           const headers = {
+                headers: new HttpHeaders(
+                    { 'Authorization': 'Bearer ' + this.getToken(),
+                               'Content-Type': 'application/json' })
+            };
+
+           return this.http.put(CRRS_API_URL + "api/room-equipment-issues", data, headers);
+        }
+
+        getToken() {
+            return this.$localStorage.retrieve('authenticationToken') || this.$sessionStorage.retrieve('authenticationToken');
+        }
+
 }

fe19e816c2da7edd55286e7c784c05ce116f81b1
Shelema Bekele <sdendaa@gmail.com>
2018-07-27 16:35:26 -0400

    ODOS_3: created the report submit forms

--- src/main/webapp/app/report-issues/report-issues.component.ts
+++ src/main/webapp/app/report-issues/report-issues.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'jhi-report-issues',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ReportIssuesComponent implements OnInit {
+    reportIssuesDetailForm:FormGroup;
+    fullName: FormControl;
+    reportDate: FormControl;
+    roomIssuesID: FormControl;
+    reportIssuesDescription: FormControl;
 
   constructor() { }
 
   ngOnInit() {
-  }
 
+    this.reportIssuesDetailForm = new FormGroup({
+      fullName: new FormControl('', Validators.required),
+      reportDate: new FormControl('', Validators.required),
+      roomIssuesID: new FormControl('', Validators.required),
+      reportIssuesDescription: new FormControl('', Validators.required)
+    });
+  }
+  SubmitRortIssues(){
+
+  }
 }
